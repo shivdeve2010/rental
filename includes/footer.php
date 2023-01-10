59cc89039406789ef30722f1d38e14bc81d6f847
shivendra1751@gmail.com <shivendra1751@gmail.com>
2023-01-10 18:37:33 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+	<li class="about">
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ea labore autem tempore quo impedit vero voluptatum aliquam laboriosam laudantium.</p>
+					<ul>
+						<li><a href="#" class="facebook" target="_blank"></a></li>
+						<li><a href="#" class="twitter" target="_blank"></a></li>
+						<li><a href="#" class="google" target="_blank"></a></li>
+						<li><a href="#" class="skype"></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+		<div class="copyrights wrapper">
+			Copyright &copy; <?php echo date("Y")?> All Rights Reserved | Designed by <a href= "https://github.com/shivdeve2010">shivdeve2010</a>
+		</div>
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
